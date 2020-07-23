ced51ff8a93773eb0941cf02aa16e7968e454b9f
Fedor Kobyakov <fedua1324@yandex.ru>
2020-07-23 16:27:17 +0300

    Done with all tasks

--- ParcticeToDoList_v2/main.js
+++ ParcticeToDoList_v2/main.js
@@ -6,36 +6,32 @@ var clearBtn = document.querySelector('.clear');
 
 var overlay = document.getElementById('overlay');
 var jokeText = document.querySelector('.jokeText'); 
-
 var jokeBtn = document.querySelector('.jokeBtn');
-jokeBtn.addEventListener('click', function() {
-    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    var request = new XMLHttpRequest();
-    request.open('GET', 'https://api.jokes.one/jod', true)
-    
-    request.onload = function () {
-        var data = JSON.parse(this.responseText);
-        console.log(data.contents.jokes[0].joke.text);
-        jokeText.innerHTML = data.contents.jokes[0].joke.text;
-    }
-    
-    request.send()
-});
+var jokeCloseBtn = document.querySelector('#jokeClosebtn');
 
-ul.addEventListener("click", function(e) {
-    console.log(e);
-    var target = e.target;
-    if(target.tagName == 'LI') {
-        target.classList.toggle('checked');
-        moveToCorrectPosition(target);
-        save();
-    }
-});
+import {jokeBtnInit, jokeCloseBtnInit, ulInit,inputInit, clearBtnInit, tipsBtnInit, closeBtnInit} from './eventListeners.js';
+import { getApiData } from "./getApiData.js";
+
+jokeBtnInit();
+jokeCloseBtnInit();
+ulInit(moveToCorrectPosition, save);
+inputInit(save, initDeleteFunctions);
+clearBtnInit(save);
+tipsBtnInit();
+closeBtnInit();
+
+var request = getApiData('https://icanhazdadjoke.com/slack');
+request.done(data => {
+    var joke = data.attachments[0].text;
+    var jokesUl = document.querySelectorAll('.tips')[1];
+    var li = document.createElement('li');
+    li.append(joke);
+    jokesUl.append(li);
+})
 
 function moveToCorrectPosition(li) {
     var firstChecked = document.querySelector('ul li.checked');
     var secondChecked = document.querySelectorAll('ul li.checked')[1];
-    console.log('hi' );
     if (li.classList.contains('checked')) {
         if (secondChecked) {
             secondChecked.before(li); //move li before secondChecked
@@ -56,7 +52,6 @@ function initDeleteFunctions() {
     for(let span of spans) {
         if (span.parentElement.tagName == 'LI')
             span.addEventListener('click', () => {
-                console.log('u clicked span');
                 span.parentElement.remove();
                 save();
             });
@@ -79,36 +74,6 @@ function load() {
         ul.innerHTML = localStorage.getItem("todos");
 }
 
-input.addEventListener("keypress", function(key) {
-    if(key.which == 13 && this.value) {
-        var li = document.createElement('li');
-        var span = document.createElement('span');
-        var icon = document.createElement('i');
 
-        var newTodo = this.value;
-        this.value = "";
-
-        icon.classList.add('fas', 'fa-trash-alt');
-        span.appendChild(icon);
-        li.append(span,newTodo);
-        ul.prepend(li);
-        initDeleteFunctions();
-        save();
-    }
-});
-
-clearBtn.addEventListener('click', function() {
-    ul.innerHTML = "";
-    save();
-});
-
-tipsBtn.addEventListener('click', function() {
-    overlay.style.height = '100%';
-});
-
-closeBtn.addEventListener('click', function(event) {
-    event.preventDefault();
-    overlay.style.height = '0';
-});
 load();
 initDeleteFunctions();
